5d7db526e528e20b2a77c5863b8eb8f5df9e9743
FajeGame <nik.slesarev.95@mail.ru>
2026-06-03 01:44:41 +0700

    удалил файлики мейна

--- src/main/kotlin/com/example/lab3/infrastructure/jpa/RestaurantJpaRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lab3.infrastructure.jpa
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RestaurantJpaRepository : JpaRepository<RestaurantEntity, Long> {
-    fun findFirstByNameIgnoreCase(name: String): RestaurantEntity?
-}
--- src/main/resources/db/migration/V1__create_users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    is_active BOOLEAN NOT NULL
-);
